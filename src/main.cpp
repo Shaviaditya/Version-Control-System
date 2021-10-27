ff5bf095e972918b30a01609a35769b4a8a4fe38
Shaviaditya <rudrashiv311@gmail.com>
2021-10-28 01:31:23 +0530

    changes in C++ code

--- src/main.cpp
+++ src/main.cpp
@@ -6,22 +6,32 @@
 #include <string>
 #include <fstream>
 #include <stdbool.h>
+#include <string.h>
 namespace fs = std::filesystem;
+void add(std::string addPath,std::string reqPath)
+{
+    if(addPath==".")
+    {
+        std::cout<<fs::current_path()<<"\n";
+        std::cout<<reqPath<<"\n";
+    } 
+}
 void init(std::string path)
 {
     struct stat *buf;
     const char *cstr = (path + "/.imperium").c_str();
     if (stat(cstr, buf))
     {
-        int check = mkdir("imperium", 0777);
+        int check = mkdir(".imperium", 0777);
         if (!check)
         {
-            int checkdir = chdir("./imperium");
+            int checkdir = chdir("./.imperium");
             if (!checkdir)
             {
                 std::ofstream add_file("add.log");
                 std::ofstream commit_file(".commit.log");
                 std::ofstream conflict("conflict");
+                mkdir(".add",0777);
                 conflict << "false\n";
                 add_file.close();
                 commit_file.close();
@@ -64,9 +74,21 @@ void init(std::string path)
     }
     printf("Repositiory Initialized\n");
 }
-int main()
+int main(int argc, char const *argv[])
 {
+    chdir(getenv("dir"));
     std::string getpath = fs::current_path();
-    init(getpath);
+    if(!strcmp("init",argv[1])){
+        std::cout<<"\nProcessing....................\n";
+        std::string ins_var = (getenv("dir"));
+        init(ins_var);
+    }
+    else if(!strcmp("add",argv[1])){
+        std::string req_path = fs::current_path();
+        add(argv[2],req_path);
+    }
+    else{
+        std::cout<<"Command entered is wrong\n";
+    }
     return 0;
 }
